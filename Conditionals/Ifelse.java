c7e1427ff32adeb2a57b2f274396b5286419c304
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-09 12:32:08 +0530

    largest value problem using if-else

--- Conditionals/Ifelse.java
+++ Conditionals/Ifelse.java
@@ -2,21 +2,44 @@ import java.util.*;
 public class Ifelse {
        public static void main(String args[]){
               Scanner sc = new Scanner(System.in);
-              System.out.println("Enter your Age");
-              int age = sc.nextInt();
 
-              if(age >= 18){
-                     System.out.println("You are Adult: Eligible for - Vote, Drive");
-              }
+
+              // System.out.println("Enter your Age");
+              // int age = sc.nextInt();
+
+              // if(age >= 18){
+              //        System.out.println("You are Adult: Eligible for - Vote, Drive");
+              // }
               
-              if(age > 13 && age < 18){
-                     System.out.println("You are Teenanger");
-              }
+              // if(age > 13 && age < 18){
+              //        System.out.println("You are Teenanger");
+              // }
               
+              // else{
+              //        System.out.println("You are Not Adult");
+              // }
+
+             
+             int A = 25;
+             int B = 29;
+
+       //       if(A >= B){
+       //         System.out.println("Value of A is greater than B");
+       //       } else {
+       //         System.out.println("Value of B is greater than A");
+       //       }
+            
+
+            //OR
+            //another way to write
+
+
+               if(A >= B)
+                System.out.println("A is greater than B");
+
               else{
-                     System.out.println("You are Not Adult");
-              }
-
+                     System.out.println("B is greater than A");
+              }     
 
        }
 }
