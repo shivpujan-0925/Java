41cb19168dfc1b788ebd7a38ec2247a07926797e
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-11 12:05:06 +0530

    switch

---
+++ Conditionals/Switch.java
@@ -0,0 +1,37 @@
+
+// #Switch statement:
+// Syntax =>
+  
+// Switch(variable){
+//   case 1 :
+//           break;
+
+//   case 2 :
+//           break;    
+//   case 3 :
+//           break;
+//   default: 
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+public class Switch{
+       public static void main(String args[]){
+              int number = 2;
+              switch(number){
+                     case 1 : System.out.println("Samosha");
+                              break;
+
+                     case 2 : System.out.println("Burger");
+                              break;
+
+                     case 3 : System.out.println("Mango shake");
+                              break;
+
+                     default:System.out.println("we realize , we are dreaming");
+                   
+
+
+              }
+       }
+}
