b76484e4c030dfdb1a126d9579de5b0926862aeb
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-12 23:15:50 +0530

    while loop

---
+++ Flow_control/While.java
@@ -0,0 +1,29 @@
+// #while loop:
+
+// Syntax;
+
+// while(condition){
+
+
+// }
+
+public class While{
+       public static void main(String args[]){
+        //    int counter = 0;
+        //    while (counter <=10 ) {
+        //       System.out.println("Hello world!");
+        //       counter++;
+        //    }
+
+      //Print 1 to 10
+
+      int number = 1;
+      while (number <= 10) {
+        System.out.println(number);
+        number++;
+        
+      }
+    
+
+       }
+}
