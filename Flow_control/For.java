839293b9b92c3ca0618bff85b635b3282f371a51
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-14 20:14:26 +0530

    reverse the given number problem

--- Flow_control/For.java
+++ Flow_control/For.java
@@ -22,14 +22,37 @@ public class For{
        //        System.out.println("****");
        // }
 
-        int line = 1;
-        while(line <=4){
-              System.out.println("****");
-              line++;
-        }
+       //  int line = 1;
+       //  while(line <=4){
+       //        System.out.println("****");
+       //        line++;
+       //  }
 
 
-       }
-  }
+       // Print reverse of Number:
+      
+       // int number = 2925;
+       // while(number > 0){
+       //        int remainder = (number % 10);
+       //        System.out.print(remainder);
+       //        number = number / 10;
+
+       // }
+       
+
+    // Reverse the given number
+      
+      int number = 10000;
+      int revNumber = 0;
+      while(number > 0){
+       int  lastDigit = number % 10;
+        revNumber = (revNumber * 10) + lastDigit;
+        number = number/10;
+
+      }
+      System.out.print(revNumber);
+
+    }
+}
     
 
