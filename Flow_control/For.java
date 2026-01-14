d1d15f6eced3398e53e3fe595c1253b4ac51f7f1
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-14 12:26:00 +0530

    For loop

---
+++ Flow_control/For.java
@@ -0,0 +1,35 @@
+// For Loop:
+
+//Syntax-
+// for(initialization; condition; updation;){
+
+
+// }
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+public class For{
+  public static void main(String args[]){
+       //    for( int i=0; i<=10; i++){
+       //        System.out.println("Hello Engineer");
+
+
+       //Print Square pattern
+
+       // for(int line=1; line <= 4; line++){
+       //        System.out.println("****");
+       // }
+
+        int line = 1;
+        while(line <=4){
+              System.out.println("****");
+              line++;
+        }
+
+
+       }
+  }
+    
+
