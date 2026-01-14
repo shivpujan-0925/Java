3f47fe7c4e07326f27dd09a2a514669187c1173e
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-14 10:54:16 +0530

    print number using while loop

--- Flow_control/While.java
+++ Flow_control/While.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 // #while loop:
 
 // Syntax;
@@ -6,9 +9,10 @@
 
 
 // }
-
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 public class While{
        public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         //    int counter = 0;
         //    while (counter <=10 ) {
         //       System.out.println("Hello world!");
@@ -17,13 +21,20 @@ public class While{
 
       //Print 1 to 10
 
-      int number = 1;
-      while (number <= 10) {
-        System.out.println(number);
-        number++;
+      // int number = 1;
+      // while (number <= 100) {
+      //   System.out.print(number+ " ");
+      //   number++;
         
-      }
-    
+      // }
+      // System.out.println();
+    System.out.println("Enter Number:");  
+    int n = sc.nextInt();
+    int counter = 1;
+    while(counter <= n){
+      System.out.print(counter+" ");
+      counter++;
+    }
 
-       }
+  }
 }
