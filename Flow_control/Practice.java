b3a8e7f576605431b0e71d35cf9d0beefd13fa6f
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-17 01:35:44 +0530

    practice problem on loop

---
+++ Flow_control/Practice.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Practice {
+       public static void main(String args[]){
+              Scanner sc = new Scanner(System.in);
+              //problem-01
+
+              // for(int i = 0; i< 5; i++){
+              //        System.out.println("Hello");
+              //        i+=2;
+              // }
+
+              //problem-02
+               
+              // int number;
+              // int choice;
+              // int evenSum = 0;
+              // int oddSum = 0;
+            
+              //     do {
+                    
+              //         number = sc.nextInt();
+                     
+              //        if(number % 2 == 0){
+              //               evenSum = evenSum + number;
+                            
+              //        }else{
+              //               oddSum = oddSum + number;
+              //        }
+              //        System.out.println("Do you want to continue? Press 1 for YES or 0 for NO");
+              //        choice = sc.nextInt();    
+   
+              // } while (choice == 1);
+              // System.out.println("Total even Value sum: " + evenSum);
+              // System.out.println("Total odd Value sum:" + oddSum);
+
+
+              //problem-03:
+              
+              // int number = sc.nextInt();
+              // int Factorial = 1;
+              // for(int i=1; i<=number; i++){
+              //         Factorial = Factorial * i;
+              // }
+              // System.out.println(Factorial);
+
+
+              //problem-04:
+              
+              // System.out.println("Enter Number:");
+              // int number = sc.nextInt();
+              //  System.out.println("Table of " + number);
+              // for( int i=1; i<=10; i++){
+                    
+              //       System.out.println(number+ "*" + i + "=" + number*i);
+              // }
+             
+
+
+
+       }
+}
