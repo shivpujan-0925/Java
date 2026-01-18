8a7c22a52257a6cf0163c788ebcb3a728f43e327
shivpujan <kumarshivpujan9087@gmail.com>
2026-01-18 12:14:52 +0530

    pattern problems

--- Pattern/Part01.java
+++ Pattern/Part01.java
@@ -32,13 +32,36 @@ public class Part01 {
       //OR
 
 
-      int n = 4;
-      for(int line = 1; line<=n; line++){
-         for(int star = 1; star <= n-line + 1; star++){
-               System.out.print("*");
-         }
-         System.out.println();
-      }
+      // int n = 4;
+      // for(int line = 1; line<=n; line++){
+      //    for(int star = 1; star <= n-line + 1; star++){
+      //          System.out.print("*");
+      //    }
+      //    System.out.println();
+      // }
+      
 
+      //  print Half-pyramid Pattern:
+
+      // for( int line =1; line <= 4; line++){
+      //    for(int num=1; num<=line; num++){
+      //        System.out.print(num);
+      //    }
+      //    System.out.println();
+      // }
+
+
+
+      // Print Character Pattern:
+
+      // int n = 4;
+      // char ch = 'A';
+      // for(int line = 1; line<=n; line++){
+      //    for(int chars= 1; chars<=line; chars++){
+      //       System.out.print(ch);
+      //       ch++;
+      //    }
+      //    System.out.println();
+      // }
    }
 }
