7eca1281a429b765eed3ec29de38953c6ba1eefe
shivpujan <kumarshivpujan9087@gmail.com>
2026-02-02 00:46:46 +0530

    numeric pyramid

--- 05_Pattern/HalfPyramid.java
+++ 05_Pattern/HalfPyramid.java
@@ -2,15 +2,45 @@ import java.util.*;
 
 public class HalfPyramid {
 
+//Inverted-half-pyramid
 
-       public static void Pyramid(int n ){
-              for(int row = 1; row <= n; row++){
-                     for(int col = 1; col <=n; col++){
-                            if(n + 1 <= row + col){
-                                   System.out.print("* ");
-                            }else{
-                                   System.out.print("__");
-                            }
+       // public static void Pyramid(int n ){
+       //        for(int row = 1; row <= n; row++){
+       //               for(int col = 1; col <=n; col++){
+       //                      if(n + 1 <= row + col){
+       //                             System.out.print("* ");
+       //                      }else{
+       //                             System.out.print("__");
+       //                      }
+       //               }
+       //               System.out.println();
+       //        }
+       // }
+
+//Inverted-half-pyramid-number
+
+       // public static void NumericPyramid(int n){
+       //     for(int row = 1; row <=n; row++){
+       //        for(int col =1; col <=n; col++){
+       //               if(row + col <= n+1){
+       //                      System.out.print(col);
+       //               }else{
+       //                      System.out.print(" ");
+       //               }
+       //        }
+       //        System.out.println();
+       //     }
+       // }
+
+       //another Logic:
+
+       public static void NumericPyramid(int n){
+            
+              for(int row = 1; row <=n; row++){
+                     for(int col = 1; col <= n - row +1; col++){
+
+                       System.out.print(col);
+
                      }
                      System.out.println();
               }
@@ -21,6 +51,9 @@ public class HalfPyramid {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-           Pyramid(n);
+        NumericPyramid(n);
+
+       //     Pyramid(n);
        }
+
 }
