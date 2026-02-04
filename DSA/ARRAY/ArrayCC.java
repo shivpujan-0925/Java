5abdecc45b9a58407d5459014c2e8fe8ea401714
shivpujan <kumarshivpujan9087@gmail.com>
2026-02-04 21:51:10 +0530

    array ip/op

---
+++ DSA/ARRAY/ArrayCC.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+public class ArrayCC {
+       public static void main(String args[]){
+           
+          int marks[] = new int[50];
+          Scanner scn = new Scanner(System.in);
+
+          System.out.println("Array length:" + marks.length);
+
+       //    System.out.println("Enter Physics marks");
+       //    marks[0] = scn.nextInt();  //phy
+       //     System.out.println("Enter Chemistry marks");
+       //    marks[1] = scn.nextInt();  //chem
+
+       //     System.out.println("Enter Maths marks");
+       //    marks[2] = scn.nextInt();  //math
+
+
+       //    System.out.println("Phy: " + marks[0]);
+       //    System.out.println("Chem: " + marks[1]);
+       //    System.out.println("Math: " + marks[2]);
+
+       //    marks[2] = 100;
+       //    marks[2] = marks[2] + 5;
+
+
+       //average:
+        
+       // int average = (marks[0] + marks[1] + marks[2]) / 3;
+
+       // System.out.println("Average: " + average);
+
+          
+              
+       }
+}
