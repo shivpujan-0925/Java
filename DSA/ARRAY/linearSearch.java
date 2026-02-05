bc6c75b653864d38e12f03f63cf807d14875888b
shivpujan <kumarshivpujan9087@gmail.com>
2026-02-05 22:41:20 +0530

    largest number

--- DSA/ARRAY/linearSearch.java
+++ DSA/ARRAY/linearSearch.java
@@ -1,26 +1,56 @@
 public class linearSearch {
        
+        //Linear Search
+    //    public static int LinearSearch(int numbers[], int key){
+    //        for(int i =0; i < numbers.length; i++){
 
-       public static int LinearSearch(int numbers[], int key){
-           for(int i =0; i < numbers.length; i++){
+    //           if(numbers[i] == key){
+    //                 return i;
+    //           }
+    //        }
+    //        return -1;
+    //    }
 
-              if(numbers[i] == key){
-                    return i;
-              }
-           }
-           return -1;
-       }
+
+    //Largest number
+
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;    //-infinity
+        int smallest = Integer.MAX_VALUE;
+
+
+        for(int i =0; i < numbers.length; i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+            }
+
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+          
+          System.out.println("Smallest number is : " + smallest);
+          return largest;
+
+    }
+
+
 
        public static void main(String args[]){
            int numbers[] = {2,4,6,8,10,12,14,16};
-           int key = 15;
-          int index =  LinearSearch(numbers, key);
+            int largest = getLargest(numbers);
 
-           if(index == -1){
-              System.out.println("NOT Found");
-           }else{
-                  System.out.println("Key is at Index : " + index);
-           }
+           System.out.println("Largest number: "+ largest);
+
+         
+        //    int key = 15;
+        //    int index =  LinearSearch(numbers, key);
+
+        //    if(index == -1){
+        //       System.out.println("NOT Found");
+        //    }else{
+        //           System.out.println("Key is at Index : " + index);
+        //    }
 
        }
 }
