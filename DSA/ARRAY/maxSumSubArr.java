31f249519240886bc60dd89cb59091176720e9b8
shivpujan <kumarshivpujan9087@gmail.com>
2026-02-11 22:47:11 +0530

    max sum of subarray

--- DSA/ARRAY/maxSumSubArr.java
+++ DSA/ARRAY/maxSumSubArr.java
@@ -2,7 +2,7 @@ public class maxSumSubArr {
 
       public static void maxSum(int numbers[]){
 
-       //brute force:
+ //BRUTE FORCE:
 
        //     int maxSum_SubArr = Integer.MIN_VALUE;
 
@@ -27,34 +27,36 @@ public class maxSumSubArr {
        //     System.out.println("Maximum sum of SubArray is : " + maxSum_SubArr);
 
 
-       //PREFIX SUM METHOD:   optimized
+//PREFIX SUM METHOD:   optimized
           
-       int currSum = 0;
-       int maxSum_subArray = Integer.MIN_VALUE;
-       int prefix[] = new int[numbers.length];
-       prefix[0] = numbers[0];
+       int maxSum = Integer.MIN_VALUE;
+       int currentSum = 0;
 
-       //calculating prefix
-       for(int i=1; i <numbers.length; i++){
-            prefix[i] = prefix[i-1] + numbers[i];
-       }
-      
+      int prefix[] = new int[numbers.length];
+      prefix[0] = numbers[0];
+
+      //calculating prefix array sum
+      for(int i=1; i< numbers.length; i++){
+         prefix[i] = prefix[i-1] + numbers[i];
+      }
+     
        for(int i=0; i < numbers.length; i++){
               int start =i;
-              for(int j = i; j<numbers.length; j++){
-                 
-                     int end = j;
-                     currSum = start ==0 ? prefix[end]: prefix[end] - prefix[start -1];
-              }
-              if(currSum > maxSum_subArray){
-                     maxSum_subArray = currSum;
-              }
+            for(int j=i; j < numbers.length; j++){
+              int end = j;
+
+              currentSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
+
+            }
+            if(currentSum > maxSum){
+              maxSum = currentSum;
+            }
+
        }
+       System.out.println("Maximum sum: " + maxSum);
 
-       System.out.println("Max Sum: " + maxSum_subArray);
-            
+
       }
-
 
        public static void main(String args[]){
            int numbers[] = {10,-2,6,-1,3};
