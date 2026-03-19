2dee61396d0eee66029e8a402db6ae7b325bcce0
Shivpujan <kumarshivpujan9087@gmail.com>
2026-03-19 07:15:54 +0530

    revise sorting

---
+++ DSA/Sorting/Bubble-sort/Test.java
@@ -0,0 +1,31 @@
+public class Test {
+    //Bubble sort
+    public static void bubbleSort(int arr[]){
+        int n = arr.length;
+        for(int turn=0;  turn<n-1;  turn++){
+            for(int j=0; j< n-1-turn; j++){
+                //swap
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                     arr[j+1] = arr[j]; 
+                     arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {5,8,2,3,9,0,1};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+
+}
