c6f3a02ea37397d330045771a9a339769a9f95b6
Shivpujan <kumarshivpujan9087@gmail.com>
2026-04-05 22:05:20 +0530

    Bits basics

---
+++ DSA/BIT-MANIPULATION/Concepts/BitManipulation.java
@@ -0,0 +1,14 @@
+
+public class BitManipulation {
+    public static void main(String[] args) {
+        // AND Operations
+        // System.out.println(5&5);
+
+        //OR Operations
+        // System.out.println(5 | 6);
+
+        //XOR Operations
+       System.out.println(5^6);
+
+    }
+}
