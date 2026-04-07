94734a3f9a5994c2a17557d809cf939809d99fd5
Shivpujan <kumarshivpujan9087@gmail.com>
2026-04-07 23:28:41 +0530

    Bit manipulation

--- DSA/BIT-MANIPULATION/Concepts/BitManipulation.java
+++ DSA/BIT-MANIPULATION/Concepts/BitManipulation.java
@@ -8,7 +8,16 @@ public class BitManipulation {
         // System.out.println(5 | 6);
 
         //XOR Operations
-       System.out.println(5^6);
+    //    System.out.println(5^6);
+
+    //Binary 1's complement
+    //   System.out.println(~10);
+
+    //Binary left shift (<<)
+    // System.out.println(5<<2);     //FORMULA-> a<<b = a * 2(to the power b)
+
+    //Binary right shift(>>)
+    // System.out.println(6>>1);     //FORMULA-> a>>b = a/2(to the power b)
 
     }
 }
