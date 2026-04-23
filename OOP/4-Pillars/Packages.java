62382025435cb42d24f1fa96aa67820a39b948d7
Shivpujan <kumarshivpujan9087@gmail.com>
2026-04-23 12:14:47 +0530

    packages in java

---
+++ OOP/4-Pillars/Packages.java
@@ -0,0 +1,18 @@
+
+import java.util.*;   //in-Built package
+
+public class Packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
+
+
+//__________________________________________________________________________________________________________
+
+ //Package in java => Package is a group of similar types of classes interfaces and sub-classes.
+
+//___________________________________________________________________________________________________________ 
+
