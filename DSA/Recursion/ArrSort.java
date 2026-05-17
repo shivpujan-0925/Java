98f6984f6a113dbff13d28860a1dd45af6e1ce50
Shivpujan <kumarshivpujan9087@gmail.com>
2026-05-17 22:12:30 +0530

    problem on arr sort using recursion

---
+++ DSA/Recursion/ArrSort.java
@@ -0,0 +1,19 @@
+public class ArrSort {
+
+    public static boolean isSorted(int arr[], int i){
+        //base case 
+        if(i == arr.length-1){
+            return true;
+        }
+
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+
+       return  isSorted(arr , i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,8,4,5};
+        System.out.println(isSorted(arr, 2));
+    }
+}
