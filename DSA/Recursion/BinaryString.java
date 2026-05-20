d2eb1f63379d984ebed74f832e12c3f70dda890a
Shivpujan <kumarshivpujan9087@gmail.com>
2026-05-20 20:47:24 +0530

    print Binary string using recursion

---
+++ DSA/Recursion/BinaryString.java
@@ -0,0 +1,23 @@
+public class BinaryString {
+    public static void printBinString(int n , int lastPlace, String str){
+
+        // base case
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+
+        //work
+         printBinString(n-1, 0, str+"0");
+
+         if(lastPlace == 0){
+             printBinString(n-1, 1, str+"1");
+         }
+
+
+    }
+    public static void main(String[] args) {
+        printBinString(3, 0, "");
+    }
+}
