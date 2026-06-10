68fe1053c7d560712f39ae6dfae30c5a009fccf5
Shivpujan <kumarshivpujan9087@gmail.com>
2026-06-10 22:07:05 +0530

    kadanes algo

--- DSA/ARRAY/KADANEs.java
+++ DSA/ARRAY/KADANEs.java
@@ -3,31 +3,22 @@ public class KADANEs {
 
 //KADANE's Algorithm.
 
-       public static void MaxSum_Subarr(int numbers[]){
-              int currSum = 0;
+       public static void  MaxSum_Subarr(int numbers[]){
               int maxSum = Integer.MIN_VALUE;
+              int currSum = 0;
+              int n = numbers.length;
 
-              for(int i = 0; i < numbers.length; i++){
-                     int start = i;
-                     currSum = start == 0 ? numbers[i] : currSum + numbers[i];
+              for(int i = 0; i < n; i++){
+                  currSum = currSum + numbers[i];
+                  if(currSum < 0){                  //kadane's algo
+                     currSum = 0;
+                  }
+                  maxSum = Math.max(maxSum, currSum);
+                 
 
-                     if(currSum <= 0){
-                            currSum = 0;
-                     }
-
-                     // if(currSum > maxSum){
-                     // maxSum = currSum;
-                     // }
-
-                     maxSum = Math.max(currSum, maxSum);
-                  
-          }
-
-            
-
-              System.out.println("Max sum: " + maxSum);
-       }   
-
+              }
+              System.out.println("Max sum of subarray : " + maxSum);
+       }
 
        public static void main(String[] args) {
 
