8bacbd1365b777b60f1289fb92e2a30ae4fa3a97
Shivpujan <kumarshivpujan9087@gmail.com>
2026-06-10 21:25:25 +0530

    prefix sum

--- DSA/ARRAY/maxSumSubArr.java
+++ DSA/ARRAY/maxSumSubArr.java
@@ -1,64 +1,71 @@
 public class maxSumSubArr {
 
-      public static void maxSum(int numbers[]){
+        //BRUTEFORCE APPROACH:
 
- //BRUTE FORCE:
+        // public static void maxSum(int numbers[]){
+        //   int maxSum = Integer.MIN_VALUE;
+        //   int currSum = 0;
+        //   int n = numbers.length;
 
-       //     int maxSum_SubArr = Integer.MIN_VALUE;
+        //   for(int i = 0; i < n; i++ ){
 
-       //     for(int i =0; i < numbers.length; i++){
+        //     int start = i;
+        //     for(int j = i + 1; j < n; j++){
+        //       currSum = 0;
+        //       int end = j;
 
-       
-       //        for(int j=i; j < numbers.length; j++){
+        //         for(int k = start; k <= end; k++){
+        //         currSum = currSum + numbers[k];
+               
+        //         }
+        //          System.out.println(currSum);
 
-       //           int currSum = 0;
-       //          for(int k=i; k <=j; k++){
-       //               System.out.print(numbers[k] + " ");
-       //               currSum = currSum + numbers[k];
-       //          }
+        //         if(maxSum < currSum){
+        //         maxSum = currSum;
+        //         }
 
-       //         if(maxSum_SubArr < currSum){
-       //               maxSum_SubArr = currSum;
-       //         } 
-            
-       //        }
-            
-       //     }
-       //     System.out.println("Maximum sum of SubArray is : " + maxSum_SubArr);
+        //     }
+        //   }
+        //   System.out.println("Maximum sum of subArr : " + maxSum);
+        // }
 
 
-//PREFIX SUM METHOD: optimized
+
+       //Optimized way : Using prefix sum
+
+       public static void maxSum(int numbers[]){
+          int n = numbers.length;
+          int maxSum = Integer.MIN_VALUE;
+          int currSum = 0;
           
-       int maxSum = Integer.MIN_VALUE;
-       int currentSum = 0;
+          //calculating prefix sum
+          int prefix[] = new int[n];
+          prefix[0] = numbers[0];
+          
+          for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+          }
+          
+          for(int i = 0; i < n; i++){
+            int start = i;
 
-      int prefix[] = new int[numbers.length];
-      prefix[0] = numbers[0];
-
-      //calculating prefix array sum
-      for(int i=1; i< numbers.length; i++){
-         prefix[i] = prefix[i-1] + numbers[i];
-      }
-     
-       for(int i=0; i < numbers.length; i++){
-              int start =i;
-            for(int j=i; j < numbers.length; j++){
+            for(int j = i+1; j < n; j++){
               int end = j;
 
-              currentSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
+              currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
+            }
 
+            if(maxSum < currSum){
+              maxSum = currSum;
             }
-            if(currentSum > maxSum){
-              maxSum = currentSum;
-            }
+          }
+          System.out.println("Maximum sum of subArray : " + maxSum);
 
        }
-       System.out.println("Maximum sum: " + maxSum);
-
-      }
+     
 
        public static void main(String args[]){
-           int numbers[] = {10,-2,6,-1,3};
+           int numbers[] = {1, -2, 6, -1, 3};
            maxSum(numbers);
        }
 
