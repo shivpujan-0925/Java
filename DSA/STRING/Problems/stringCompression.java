4e5bcaf9d84dff96d385cbd6f6073569dd0c4fc5
Shivpujan <kumarshivpujan9087@gmail.com>
2026-07-08 09:35:02 +0530

    Add string compression algorithm implementation

---
+++ DSA/STRING/Problems/stringCompression.java
@@ -0,0 +1,26 @@
+package Problems;
+
+public class stringCompression {
+
+    public static String compressed(String str){
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i < str.length(); i++){
+            int count = 1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                 count++;
+                 i++;
+            }
+            sb.append(str.charAt(i));
+
+            if(count > 1){
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "raaadheee";
+        System.out.println(compressed(str));
+
+    }
+}
