e8519287cec83cfc0eb38c8e9070b4893ba949f4
Shivpujan <kumarshivpujan9087@gmail.com>
2026-07-31 09:04:41 +0530

    Refactor Fibbonacci class for improved readability and maintainability

--- DSA/Recursion/Fibbonacci.java
+++ DSA/Recursion/Fibbonacci.java
@@ -1,18 +1,24 @@
 public class Fibbonacci {
-    public static int fibo(int n){
-      
-        if(n == 0 || n == 1 ){
-            return n;
-        }
+  public static int fibo(int n) {
+     //base case
+     if(n == 0 || n == 1){
+        return n;
+     }
 
-        int Fn_1 = fibo(n-1);
-        int Fn_2 = fibo(n-2);
+     //0th fibbonacci value
+     int fibN_2 = fibo(n-2);
 
-        int Fn = Fn_1 + Fn_2;
-        return Fn;
-    }
+     //1st fibbonacci value
+     int fibN_1 = fibo(n-1);
+
+    //final fibbonacci value
+     int fibN = fibN_2 + fibN_1;
+     return fibN;
+     
+  
+  }
     public static void main(String[] args) {
-        int n = 25;
+        int n = 99;
       System.out.println(fibo(n));
     }
     
