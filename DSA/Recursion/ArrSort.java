00fd96038c47022c27b0860872881017a083b548
Shivpujan <kumarshivpujan9087@gmail.com>
2026-07-31 19:19:43 +0530

    Refactor ArrSort class for improved readability and clarity in isSorted method

--- DSA/Recursion/ArrSort.java
+++ DSA/Recursion/ArrSort.java
@@ -1,19 +1,23 @@
 public class ArrSort {
+   public static boolean isSorted(int arr[], int i){
 
-    public static boolean isSorted(int arr[], int i){
-        //base case 
-        if(i == arr.length-1){
-            return true;
-        }
+    //problem: check given array is SORTED or Not.
+    int n = arr.length;
+      //base case
+       if(i == n-1){
+        return true;
+       }
 
-        if(arr[i] > arr[i+1]){
-            return false;
-        }
+      if(arr[i] > arr[i+1] ){
+        return false;
+      }
+      //recursive calling
+      return isSorted(arr, i+1);
 
-       return  isSorted(arr , i+1);
-    }
+
+   }
     public static void main(String[] args) {
-        int arr[] = {1,2,8,4,5};
-        System.out.println(isSorted(arr, 2));
+        int arr[] = {1,2,3,4,5,3};
+        System.out.println(isSorted(arr, 0));
     }
 }
