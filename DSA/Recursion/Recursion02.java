f88eb1cda1e359cfaaa41fe02ffda9d379cc7227
Shivpujan <kumarshivpujan9087@gmail.com>
2026-07-31 06:26:18 +0530

    Refactor Recursion02 class to uncomment and clean up the Fact and sum methods for better readability

--- DSA/Recursion/Recursion02.java
+++ DSA/Recursion/Recursion02.java
@@ -2,32 +2,28 @@ public class Recursion02 {
     
     //calculate factorial
     //____________________________________
+    public static int Fact(int n){
+        //base case
+        if(n == 0){            //factorial of '0' is 1
+            return 1;
+        } 
 
-    // public static int Fact(int n ){
-    //     //Base case
-    //     if(n == 0){
-    //         return 1;
-    //     }
-
-    //     int Fn_1 = Fact(n-1);
-    //     int Fn = n * Fn_1;
-
-    //     return Fn;
-    // }
+        int FN_1 = Fact(n-1);    //finding factorial for (n-1), firstly
+        int FN = n * FN_1;      //final result of factorial.
+        return FN;
+    }
 
 //---------------------------------------------------------------------------------
 
   //Sum of N natural number
 
-  public static int sum(int n){
-    if(n == 1){
-        return 1;
-    }
-    int prevSum = sum(n-1);
-    int totalSum = n + prevSum;
-    return totalSum;
-  }
+    public static int sum(int n){
+        if(n == 1) return 1;  //base case
 
+        int prevSum = sum(n-1);
+        int Sum = n + prevSum;
+        return Sum;
+    }
     public static void main(String[] args) {
         int n = 5;
     //   System.out.println(Fact(n));
