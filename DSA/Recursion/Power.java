8674e670f8123e7b8e7758035d82800a975a6f5a
Shivpujan <kumarshivpujan9087@gmail.com>
2026-08-03 17:52:05 +0530

    Refactor Power class for improved readability and clarity in power and optimizedPower methods

--- DSA/Recursion/Power.java
+++ DSA/Recursion/Power.java
@@ -1,16 +1,17 @@
 public class Power {
 
-    public static int power(int x, int n ){
-
+    public static int power(int x, int n){
         //base case
-        if(n == 0){
-            return 1;
-        }
-        int xnm1 = power(x, n-1);
-        int xn = x * xnm1;
-        return xn;
-    }
+        if(n == 0) return 1;
+
+        // int XN_1 = power(x, n-1);
+        // return x * XN_1;
+
+        return x * power(x, n-1);
    
+    }
+
+
     //optimized code 
 
     public static int optimizedPower(int a , int n ) {
@@ -20,25 +21,25 @@ public class Power {
         }
 
     //    int halfsqrPower =  optimizedPower(a, n/2) * optimizedPower(a, n/2);    ==> not optimized
+     
+    int halfPower = optimizedPower(a, n/2);  //recursive calling with n/2 value of power
+    int halfPowersqr = halfPower * halfPower;
 
-    int halfPower = optimizedPower(a, n/2);
-    int halfsqrPower = halfPower * halfPower;
+    //n is odd 
+    if(n % 2 != 0){
+        halfPowersqr = a * halfPowersqr;
+    }
+    return halfPowersqr;
 
-       //n is odd 
-       if(n % 2 != 0){
-        return a * halfsqrPower;
-       }
-
-       return halfsqrPower;
     }
 
     public static void main(String[] args){
-        // int x = 3;
-        // int n = 9;
+    //     int x = 2;
+    //     int n = 10;
     //  System.out.println(power(x,n));
 
     int a = 2;
-    int n = 5;
+    int n = 9;
     System.out.println(optimizedPower(a, n));
 
     }
