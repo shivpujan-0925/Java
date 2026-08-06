f94df05c7255bc4a8c3605c5851abeb9085c6a4c
Shivpujan <kumarshivpujan9087@gmail.com>
2026-08-06 22:52:07 +0530

    Refactor FriendsPair class for improved readability and clarity in friendsPairing method

--- DSA/Recursion/FriendsPair.java
+++ DSA/Recursion/FriendsPair.java
@@ -1,26 +1,27 @@
 public class FriendsPair {
 
     public static int friendsPairing(int n){
-        //base case
-        if(n == 1 || n == 2){
-            return n;
-        }
-        // //choice
-        // //single
-        // int singleWays = friendsPairing(n-1);
+      //base case
+      if(n == 1 || n == 2){
+        return n;
+      }
+
+    //   //choice 
+    //   //-single
+    //   int singlePair = friendsPairing(n-1);
+
+    //   //-paired
+    //   int pairChoice = friendsPairing(n-2);
+    //   int pairedTotal = (n-1)*pairChoice;
+
+    //   int totalWays = singlePair + pairedTotal;
+    //   return totalWays;
     
-        // //paired
-        // int pairedWays = friendsPairing(n-1) * friendsPairing(n-2);
+    //other way to write :
+    return friendsPairing(n-1) + (n-1)*friendsPairing(n-2);
 
-        // //Total ways
-        // int totalWays = singleWays + pairedWays;
-        // return totalWays;
-
-
-        //other way to write code
-        return friendsPairing(n-1) + (n-1)*friendsPairing(n-2);
     }
     public static void main(String[] args) {
-        System.out.println(friendsPairing(3));
+        System.out.println(friendsPairing(5));
     }
 }
