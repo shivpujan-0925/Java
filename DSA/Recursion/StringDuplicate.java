d9cab3ce3cc1f6d051ceb130682402e9ec159428
Shivpujan <kumarshivpujan9087@gmail.com>
2026-08-06 19:03:11 +0530

    Refactor StringDuplicate class for improved readability and clarity in removeDuplicate method

--- DSA/Recursion/StringDuplicate.java
+++ DSA/Recursion/StringDuplicate.java
@@ -1,28 +1,29 @@
 
 public class StringDuplicate {
     public static void removeDuplicate(String str, int idx, StringBuilder newString, boolean map[]){
-        //base case
-        if(idx == str.length()){
+         //base case
+         if(idx == str.length()-1){
             System.out.println(newString);
             return;
-        }
+         }
+         //traverse the character of str. 
+         char currChar = str.charAt(idx);
 
-        // working
-        char currChar = str.charAt(idx);
-        if(map[currChar-'a'] == true){    //check duplicate
+         if(map[currChar - 'a'] == true){        //check char already present in map or NOT.
             removeDuplicate(str, idx+1, newString, map);
-        }else {
-            map[currChar - 'a'] = true;    //asign true 
-            removeDuplicate(str, idx+1, newString.append(currChar), map);
-        }
+         }else{
+            map[currChar - 'a'] = true;   //if char not present in map then set true 
+            removeDuplicate(str, idx+1, newString.append(currChar), map);   //calling removeDuplicate fun with append new char in newString
+         }
 
 
     }
     public static void main(String[] args) {
-        String str = "inddianaa";
-        StringBuilder newStr = new StringBuilder("");
-        boolean map[] = new boolean[26];
+      String str = "appnnacollege";
+      int idx = 0;
+      boolean map[] = new boolean[26];
+      StringBuilder newString = new StringBuilder("");
 
-        removeDuplicate(str, 0, newStr, map);
+      removeDuplicate(str, idx , newString, map);
     }
 }
