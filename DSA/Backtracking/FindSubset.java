755d9fbb46903e08714b5aabf0ebe89dbe1c9dd3
Shivpujan <kumarshivpujan9087@gmail.com>
2026-09-21 20:32:05 +0530

    implement subset generation using backtracking

---
+++ DSA/Backtracking/FindSubset.java
@@ -0,0 +1,24 @@
+public class FindSubset {
+    public static void findSubsets(String str, String ans, int i){
+        //base case
+          if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            }else{
+                System.out.println(ans);
+            }
+
+            return;
+          }
+
+        //choice:YES
+        findSubsets(str, ans + str.charAt(i), i+1);
+
+        //choice:NO
+        findSubsets(str, ans, i+1);
+
+    }
+    public static void main(String[] args) {
+        findSubsets("abc", "", 0);
+    }
+}
