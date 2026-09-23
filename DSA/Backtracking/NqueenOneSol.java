136c89ef33a8d152b10e7a34d430204b7577dfa6
Shivpujan <kumarshivpujan9087@gmail.com>
2026-09-23 09:32:44 +0530

    implement N-Queens solution using backtracking

---
+++ DSA/Backtracking/NqueenOneSol.java
@@ -0,0 +1,87 @@
+public class NqueenOneSol {
+     public static boolean isSafe(char board[][], int row, int col){
+        //check:vertically up
+        for(int i=row-1; i>=0; i--){
+           if(board[i][col] == 'Q'){
+              return false;
+           }
+        }
+
+        //check: diag, left up
+        for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        //check: diag, right up
+        for(int i = row-1, j = col+1; i >= 0 && j < board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean nQueens(char board[][], int row){
+            //base case:
+            if(row == board.length){
+                // printChess(board);
+                count++;
+                return true;
+            }
+            
+            for(int j = 0; j < board.length; j++){
+
+                if(isSafe(board, row, j)){
+                  board[row][j] = 'Q';
+                  if(nQueens(board, row+1)){
+                    return true;
+                  }    //recursive step
+                  board[row][j] = 'x';      //backtraping step.
+                }
+
+            }
+            return false;
+        
+    }
+
+    //print chess:
+    public static void printChess(char board[][]){
+        System.out.println("___CHESS BOARD____");
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+            
+
+        }
+       
+        System.out.println();
+    }
+
+    static int count = 0;
+    public static void main(String[] args) {
+        int n = 5;
+        char board[][] = new char[n][n];
+
+        //initialize
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                board[i][j] = 'x';
+            }
+        }
+        
+       if( nQueens(board, 0)){
+        System.out.println("Solution is possible");
+        printChess(board);
+       }else{
+        System.out.println("Solution is not possible");
+       }
+
+
+        // System.out.println("Total ways to solve " + n + " Queens : " + count);
+    }
+}
